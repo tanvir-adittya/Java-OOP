6dccf04c725557ff3eb2fdb6eed7e0d5f02954ed
tanvir-adittya <tanvirhossainadittya@gmail.com>
2025-07-04 17:24:44 +0600

    compile time polymorphism

---
+++ Animal3.java
@@ -0,0 +1,15 @@
+public class Animal3 {
+    
+    public void eat() {
+        System.out.println("Animal is eating");
+    }
+
+    public void eat(int tm) {
+        System.out.println("Animal is eating " + tm + " times");
+    }
+
+    public void eat(String fd) {
+        System.out.println("Animal is eating " + fd);
+    }
+
+}
---
+++ Tester9.java
@@ -0,0 +1,11 @@
+public class Tester9 {
+    public static void main(String[] args) {
+        
+        Animal3 a1 = new Animal3();
+
+        a1.eat();
+        a1.eat(3);
+        a1.eat("bread");
+
+    }
+}
